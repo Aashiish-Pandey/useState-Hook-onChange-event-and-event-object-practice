00c5081def8f8e63abd808fa1cefc2206397a138
Aashiish-Pandey <ash006pandey@gmail.com>
2020-09-25 11:43:52 +0530

    outputing list

--- src/App.js
+++ src/App.js
@@ -52,25 +52,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            click={() => this.switchNameHandler("Akhilesh Pandey")}
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            changed={this.namechangeHandler}
-          >
-            My Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person) => {
+            return <Person name={person.name} age={person.age}></Person>;
+          })}
         </div>
       );
     }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
