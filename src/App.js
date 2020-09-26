d447d16b861c1eb7aae3a382c0f691cfd8ad1fea
Aashiish-Pandey <ash006pandey@gmail.com>
2020-09-26 21:29:21 +0530

    setting style dynamically

--- src/App.js
+++ src/App.js
@@ -52,7 +52,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "yellow",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -77,6 +78,7 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = "red";
     }
 
     return (
