9dff5bbaa1701bfd8673c96e1628dabfa5a1d753
Aashiish-Pandey <ash006pandey@gmail.com>
2020-09-27 00:55:58 +0530

    Setting CSS classes Dynamically

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
+import "./Person.css";
 
-const person = ( props ) => {
-    return (
-        <div>
-            <p onClick ={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type = "text" onChange ={props.changed} />
-        </div>
-    )
+const Person = (props) => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} />
+    </div>
+  );
 };
 
-export default person;
+export default Person;
